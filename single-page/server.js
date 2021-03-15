0305465d14b9c77f3d80303c54bc470be52236ba
dpr white <dpr.white@protonmail.com>
2021-03-14 20:03:26 -0400

    http only

--- single-page/server.js
+++ single-page/server.js
@@ -93,29 +93,6 @@ async function main() {
 
   // start https server, falling back to http if https fails
   console.log('starting express');
-  try {
-    const tls = {
-      cert: fs.readFileSync(config.sslCrt),
-      key: fs.readFileSync(config.sslKey),
-    };
-    httpsServer = https.createServer(tls, expressApp);
-    httpsServer.on('error', (e) => {
-      console.error('https server error,', e.message);
-    });
-    await new Promise((resolve) => {
-      httpsServer.listen(config.httpPort, config.httpIp, () => {
-        console.log(`server is running and listening on ` +
-                    `https://${config.httpIp}:${config.httpPort}`);
-        resolve();
-      });
-    });
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      console.error('no certificates found (check config.js)');
-      console.error('  could not start https server ... trying http');
-    } else {
-      err('could not start https server', e);
-    }
     expressApp.listen(config.httpPort, config.httpIp, () => {
       console.log(`http server listening on port ${config.httpPort}`);
     });
